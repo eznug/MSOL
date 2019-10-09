76b00d53b0c6b94e34b4d7878838848ff8cbf0a9
Pettersson <46201528+eznug@users.noreply.github.com>
2019-10-09 15:21:36 +0200

    Update MSOL-AddUser.ps1
    Added description row 7 & 8

--- MSOL-AddUser.ps1
+++ MSOL-AddUser.ps1
@@ -1,3 +1,28 @@
-﻿##Set-ExecutionPolicy Unrestrictedconnect-msolservice$testcsv = import-csv C:\script\User.csv -Delimiter ";"  ##-Delimiter :;, blank tab    foreach($test in $testcsv)     {     $UPN = $test.UserPrincipalName      $DN = $test.DisplayName     $FN = $test.FirstName   $LN = $test.LastName 
+﻿##Set-ExecutionPolicy Unrestricted
 
-    New-MsolUser -UserPrincipalName $UPN -DisplayName $DN -FirstName $FN -LastName $LN -UsageLocation SV -LicenseAssignment "afakedomain:ENTERPRISEPREMIUM"      }  
+connect-msolservice
+
+$testcsv = import-csv C:\script\User.csv -Delimiter ";"
+  ##-Delimiter :;, blank tab
+  ## The output-file with users should in this case be stored in C:\script\ with the filename User.csv with ; as a delimiter
+  ## if the outputfile has a different delimiter, change row 5
+ 
+  
+ foreach($test in $testcsv)
+  
+   {
+  
+   $UPN = $test.UserPrincipalName
+   
+   $DN = $test.DisplayName
+  
+   $FN = $test.FirstName
+
+   $LN = $test.LastName
+
+ 
+
+    New-MsolUser -UserPrincipalName $UPN -DisplayName $DN -FirstName $FN -LastName $LN -UsageLocation SV -LicenseAssignment "afakedomain:ENTERPRISEPREMIUM"
+  
+    }
+  
